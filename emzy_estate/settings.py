70f27463ff45f08699c27bfd9e9635222b612d6f
Chichebe <chichebedarlington@gmail.com>
2026-02-05 18:25:21 +0100

    settings

--- emzy_estate/settings.py
+++ emzy_estate/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 import dj_database_url
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')  #'django-insecure-4lp$50ds@)y*0lo+7%qp!=yar)s1#5fv&cvhx&(dw(znt(5x!g'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(',')
 
 
 # Application definition
@@ -89,7 +90,7 @@ DATABASES = {
 }
 
 database_url = os.environ.get("DATABASE_URL")
-DATABASES["default"] = dj_database_url.parse(database_url)
+DATABASES["default"] = dj_database_url.parse(config("DATABASE_URL"))
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
