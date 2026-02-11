f07835bc72476486edd03722decc8523acd74f31
Chichebe <chichebedarlington@gmail.com>
2026-02-11 14:59:58 +0100

    hard

--- emzy_estate/settings.py
+++ emzy_estate/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = ['https://emzy-estate.onrender.com', 'emzy-estate.onrender.com', 'localhost','127.0.0.1']
+ALLOWED_HOSTS = ['https://emzy-estate.onrender.com/', 'emzy-estate.onrender.com', 'localhost','127.0.0.1']
 # ALLOWED_HOSTS = (
 #     ["127.0.0.1", "localhost"]
 #     if ENVIRONMENT == "development"
